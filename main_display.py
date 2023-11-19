e0c443b50936cd4458e54f77cf1250225480c097
Rowan Jeffery-Wall <rowjw97@gmail.com>
2023-11-19 16:25:27 +0000

    Implemented basic line layout.

--- main_display.py
+++ main_display.py
@@ -2,18 +2,22 @@
 import sys
 import os
 import logging
-from waveshare_epd import epd5in83b_V2
+from waveshare_epd import epd5in83_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 
+#Pictures and fonts will be stored here.
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+print(picdir)
+
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    logging.info("epd5in83b_V2 Demo")
+    logging.info("Display refreshing")
     
     # Display clearing and initialisation done here.
-    epd = epd5in83b_V2.EPD()
+    epd = epd5in83_V2.EPD()
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
@@ -26,38 +30,29 @@ try:
     
     # Drawing on the Horizontal image
     logging.info("1.Drawing on the Horizontal image...") 
-    HBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 648*480
-    HRYimage = Image.new('1', (epd.width, epd.height), 255)  # 648*480  HRYimage: red or yellow image  
+    HBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 648*480 
     drawblack = ImageDraw.Draw(HBlackimage)
-    drawry = ImageDraw.Draw(HRYimage)
-    drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
-    drawblack.text((10, 20), 'Transport dashboard', font = font24, fill = 0)    
-    drawblack.line((20, 50, 70, 100), fill = 0)
-    drawblack.line((70, 50, 20, 100), fill = 0)
-    drawblack.rectangle((20, 50, 70, 100), outline = 0)    
-    drawry.line((165, 50, 165, 100), fill = 0)
-    drawry.line((140, 75, 190, 75), fill = 0)
-    drawry.arc((140, 50, 190, 100), 0, 360, fill = 0)
-    drawry.rectangle((80, 50, 130, 100), fill = 0)
-    drawry.chord((200, 50, 250, 100), 0, 360, fill = 0)
+
+    # Layout variables:
+    half_width = epd.width / 2
+    banner_height = epd.height * 0.2
+    main_height = epd.height * 0.8
+    half_main_height = (main_height / 2) + banner_height
+ 
+    # Drawing basic layout.
+    drawblack.line((0, banner_height, epd.width, banner_height), fill = 0, width = 1)
+    drawblack.line((half_width, banner_height, half_width, epd.height), fill = 0, width = 1)  
+    drawblack.line((0, half_main_height, epd.width, half_main_height), fill = 0, width = 1)
+
+    
+    
     # Drawn image is saved to a buffer.
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+    epd.display(epd.getbuffer(HBlackimage))
     time.sleep(2)
     
-    # This shows the rationale for displaying images from a file.
-    logging.info("3.read bmp file")
-    HBlackimage = Image.open(os.path.join(picdir, '5in83b_V2_b.bmp'))
-    HRYimage = Image.open(os.path.join(picdir, '5in83b_V2_r.bmp'))
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
-    time.sleep(2)
-    
-    # Below code is used to clear and sleep the display. This probably won't be neccessary.
-    #logging.info("Clear...")
-    #epd.init()
-    #epd.Clear()
-    
-    #logging.info("Goto Sleep...")
-    #epd.sleep()
+    # Display goes to sleep.
+    logging.info("Goto Sleep...")
+    epd.sleep()
         
 except IOError as e:
     logging.info(e)
