14360e700d4fcd75ebc63311115f284f61b7fc26
Rowan Jeffery-Wall <rowjw97@gmail.com>
2023-11-25 19:12:24 +0000

    Implemented basic function for getting stop info.

---
+++ transport_getters.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+from dataclasses import dataclass
+import logging
+import requests
+import json
+
+logging.basicConfig(level=logging.DEBUG)
+
+@dataclass
+class Stop(object):
+    name: str
+    type: str
+    code: str
+
+def get_arrival_predictions(stop_id):
+    logging.info(f"Pulling arrival info for {stop_id}.")
+    url = f"https://api.tfl.gov.uk/StopPoint/{stop_id}/Arrivals"
+
+    request = requests.get(url)
+    return request.json()
+
+def get_stop_information(stop_id):
+    logging.info(f"Pulling stop details for {stop_id}")
+    url = f"https://api.tfl.gov.uk/StopPoint/{stop_id}"
+
+    request = requests.get(url)
+    data = request.json()
+
+    stop_name = data["commonName"]
+    child_stop = find_child_stop(data["children"], stop_id)
+    stop_type = child_stop["modes"]
+    stop_code = child_stop["stopLetter"]
+
+    return Stop(stop_name, stop_type, stop_code)
+
+def find_child_stop(children, stop_id): # TfL supplies a list of children for specific bus stops, etc. We need to find the correct child from the StopPoint data.
+    child_ids = [child["naptanId"] for child in children]
+    child_index = child_ids.index(stop_id)
+    return children[child_index]
+
+if __name__ == "__main__":
+    print(get_stop_information("490006335N"))
