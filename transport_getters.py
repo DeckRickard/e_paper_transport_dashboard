61158d75b8f0e66618aec4029d8c2d166548871b
Rowan Jeffery-Wall <rowjw97@gmail.com>
2023-12-02 11:47:15 +0000

    Added logic to deal with non-bus stops.

--- transport_getters.py
+++ transport_getters.py
@@ -54,9 +54,11 @@ def get_stop_information(stop_id):
 
     stop_name = data["commonName"]
     child_stop = find_child_stop(data["children"], stop_id)
-    stop_type = child_stop["modes"]
-    stop_code = child_stop["stopLetter"]
-
+    stop_type = child_stop["modes"][0]
+    if stop_type == 'bus':
+        stop_code = child_stop["stopLetter"]
+    else:
+        stop_code = ''
     return Stop(stop_name, stop_type, stop_code)
 
 def find_child_stop(children, stop_id): # TfL supplies a list of children for specific bus stops, etc. We need to find the correct child from the StopPoint data.
