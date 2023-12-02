9c2b941b119c51f14b573e56a997f23d633c091d
Rowan Jeffery-Wall <rowjw97@gmail.com>
2023-12-02 11:00:02 +0000

    Implemented function to get arrival predictions.

--- transport_getters.py
+++ transport_getters.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 import logging
 import requests
 import json
@@ -16,14 +17,33 @@ class Stop(object):
 class Arrival(object):
     line: str
     destination: str
-    time_to_arrival: str
+    time_to_arrival: int
+    formatted_arrival_time: str
 
 def get_arrival_predictions(stop_id):
     logging.info(f"Pulling arrival info for {stop_id}.")
     url = f"https://api.tfl.gov.uk/StopPoint/{stop_id}/Arrivals"
 
     request = requests.get(url)
-    return request.json()
+    data = request.json()
+    arrivals = []
+
+    for prediction in data:
+        arrivals.append(Arrival(prediction["lineName"], prediction["destinationName"], prediction["timeToStation"], format_predicted_time(prediction["timeToStation"])))
+    
+    arrivals.sort(key=lambda x: x.time_to_arrival, reverse=False) # This sorts the list in ascending order by arrival time.
+    return arrivals
+
+def format_predicted_time(arrival_time):
+    arrival_minutes = int(arrival_time / 60)
+
+    if arrival_minutes == 0:
+        return "due"
+    elif arrival_minutes < 30: # Predicted arrivals sooner than 30 minutes are displayed as minutes, longer than that will display a timestamp.
+        return str(arrival_minutes)
+    else:
+        current_time = datetime.now()
+        return (current_time + timedelta(minutes=arrival_minutes)).strftime('%H:%M')
 
 def get_stop_information(stop_id):
     logging.info(f"Pulling stop details for {stop_id}")
@@ -46,3 +66,4 @@ def find_child_stop(children, stop_id): # TfL supplies a list of children for sp
 
 if __name__ == "__main__":
     print(get_stop_information("490006335N"))
+    print(get_arrival_predictions("490006335N"))
